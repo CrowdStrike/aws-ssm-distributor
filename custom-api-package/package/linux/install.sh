0f9cb0e91691d513ac37330454872929eabb64eb
ffalor <35144141+ffalor@users.noreply.github.com>
2025-05-12 14:20:15 -0500

    add new linux install script

--- custom-api-package/package/linux/install.sh
+++ custom-api-package/package/linux/install.sh
@@ -1,272 +1,195 @@
 #!/bin/bash
-
-echoRed() {
-    echo -e "\033[0;31m$1\033[0m"
-    echo ""
-}
-
-jsonValue() {
-    KEY=$1
-    num=$2
-    awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'"$KEY"'\042/){print $(i+1)}}}' | tr -d '"' | sed -n "${num}p"
-}
-
-usage() {
-    cat << EOF
-Script to download and install the CrowdStrike Falcon sensor.
-
-Usage:
-This script supports two modes of execution and two methods of ingesting variables.
-Using parameter arguments:
-   cssensor_install <CLIENT_ID> <CLIENT_SECRET> <CLIENT_CID> <INSTALL_PARAMS> <INSTALL_TOKEN>
-or
-   cssensor_install <CLIENT_OATH_TOKEN> <CLIENT_CID> <INSTALL_PARAMS> <INSTALL_TOKEN>
-
-Using environment variables:
-   export CS_FALCON_OAUTH_TOKEN={TOKEN_HERE}
-   export CS_FALCON_CID={CID_HERE}
-   cssensor_install
-
-Parameter Arguments:
-  CLIENT_ID: This is the client_id for the CrowdStrike API Credentials.
-  CLIENT_SECRET: This is the client_secret for the CrowdStrike API Credentials.
-  CLIENT_CID: This is the Customer CID for the CrowdStrike Falcon API.
-  CLIENT_OAUTH_TOKEN: This is the token returned post authentication to the API. 
-    If this parameter is passed, CLIENT_ID and CLIENT_SECRET should not be provided.
-  
-Environment Variables:
-  CS_FALCON_OAUTH_TOKEN: Required unless CS_FALCON_CLIENT_ID and CS_FALCON_CLIENT_SECRET are provided
-    This is the OAUTH2 token from the CrowdStrike API
-  CS_FALCON_CLIENT_ID: Required if CS_FALCON_OAUTH_TOKEN is unset.
-    This is the client_id for the CrowdStrike API Credentials. Needs at least "Sensor Download" permissions.
-  CS_FALCON_CLIENT_SECRET: Required if CS_FALCON_OAUTH_TOKEN is unset.
-    This is the client_secret for the CrowdStrike API Credentials. Needs at least "Sensor Download" permissions.
-  CS_FALCON_CID: This is the Customer CID for the CrowdStrike Falcon API.
-  CS_INSTALL_PARAMS will set installer parameters
-  CS_INSTALL_TOKEN will set the provisioning token
-
-AWS Systems Manager (SSM) integration:
-  SSM_AUTH_TOKEN will override the value of CS_FALCON_OAUTH_TOKEN.
-  SSM_CID will override the value of CS_FALCON_CID
-  SSM_NCNT will set the "N-" version count. (2 = N-2 version)
-  SSM_LINUXINSTALLPARAMS will set installer parameters, overriding environment / parameter values
-  SSM_INSTALLTOKEN will set the provisioning token, overriding environment / parameter values
-EOF
-}
-
-userAgent="crowdstrike-custom-api-distributor-package/v1.0.0"
-
-osDetail(){
-  UN=$(uname -s)
-  if [[ "$UN" == "Darwin" ]]
-  then
-	OS_NAME="Darwin"
-	OS_VERSION=$(uname -r)
-  else
-	OS_NAME=$(cat /etc/*release | grep NAME= | awk '!/CODENAME/ && !/PRETTY_NAME/' | awk '{ print $1 }' | awk -F'=' '{ print $2 }' | sed "s/\"//g")
-	OS_NAME=$(echo $OS_NAME | awk '{ print $1 }')
-	OS_VERSION=$(cat /etc/*release | grep VERSION_ID= | awk '{ print $1 }' | awk -F'=' '{ print $2 }' | sed "s/\"//g")
-  fi
-}
-
-rpmInstall(){
-   yum install libnl -y
-   rpm -ivh $1
-   /opt/CrowdStrike/falconctl -s -f --cid=$2 $3 $4
-   systemctl restart falcon-sensor
-   rm $1
-}
-
-aptInstall(){
-   # May need to move this over to snap for Ubuntu 20
-   apt-get -y install libnl-genl-3-200 libnl-3-200
-   sudo dpkg -i $1
-   apt-get -y --fix-broken install
-   /opt/CrowdStrike/falconctl -s -f --cid=$2 $3 $4
-   service falcon-sensor restart
-   rm $1
-}
-
-if [ "$1" = "-h" ]; then
-    usage
-    exit
+if pgrep  -u root falcon-sensor >/dev/null 2>&1 ; then
+  echo "Falcon Sensor already installed... if you want to update or downgrade, please use Sensor Update Policies in the CrowdStrike console. Please see: https://falcon.crowdstrike.com/documentation/66/sensor-update-policies for more information."
+  exit 0
 fi
 
-NCNT=${SSM_NCNT}
-if [ -z "$NCNT" ]
-then
-  NCNT=0
-fi
-CS_FALCON_OAUTH_TOKEN=${CS_FALCON_OAUTH_TOKEN}
-if ! [ -z "$SSM_AUTH_TOKEN" ]
-then
-    CS_FALCON_OAUTH_TOKEN=${SSM_AUTH_TOKEN}
-fi
-CS_FALCON_CLIENT_ID=${CS_FALCON_CLIENT_ID}
-CS_FALCON_CLIENT_SECRET=${CS_FALCON_CLIENT_SECRET}
-CS_INSTALL_PARAMS=${CS_INSTALL_PARAMS}
-CS_INSTALL_TOKEN=${CS_INSTALL_TOKEN}
-if ! [ -z "$SSM_CID" ]
-then
-    CS_FALCON_CID=${SSM_CID}
+# Error Handling
+function errout {
+    rc="$?"
+    echo "[ERROR] Falcon Sensor installation failed with $rc while executing '$2' on line $1"
+    exit "$rc"
+}
+
+trap 'errout "${LINENO}" "${BASH_COMMAND}" ' ERR
+
+# Echo to stderr
+function errcho {
+    >&2 echo ${@}
+    exit 1
+}
+
+#For logging the commands we run.  NOTE: remember not to log URLs or tokens!
+function RUN {
+    echo "info: Running [$*]"
+    "$@"
+}
+
+function EncodeQueryParameter {
+  QueryParameter="$1"
+  QueryParameter="${QueryParameter//+/%2b}"
+  QueryParameter="${QueryParameter//\'/%27}"
+  QueryParameter="${QueryParameter//:/%3a}"
+  QueryParameter="${QueryParameter// /%20}"
+  echo "$QueryParameter"
+}
+
+# Starting
+echo 'Installing Falcon Sensor...'
+
+# Parameters
+INSTALLER_LOC="/tmp"
+
+# Get config params
+echo 'Getting required config params ...'
+
+if [[ -z "$SSM_CS_CCID" ]]; then
+    errcho "Missing required param SSM_CS_CCID. If the required parameter was passed to the package, ensure the target instance has a ssm agent version of 2.3.1550.0 or greater installed."
+    exit 1
 fi
 
-if ! [ -z "$3" ] || ! [ -z "$4" ]
-then
-    CS_FALCON_OAUTH_TOKEN=$1
-    CS_FALCON_CID=$2
-    CS_INSTALL_PARAMS=$3
-    CS_INSTALL_TOKEN="--provisioning-token=$4"
+proxy=""
+proxy_name=""
+proxy_port=""
+
+CCID="${SSM_CS_CCID}"
+INSTALLTOKEN="${SSM_CS_INSTALLTOKEN}"
+SSM_CS_LINUX_INSTALLPARAMS="${SSM_CS_LINUX_INSTALLPARAMS:-}"
+SSM_CS_LINUX_INSTALLPARAMS="${SSM_CS_LINUX_INSTALLPARAMS//\`/\\\`}"
+SSM_CS_LINUX_INSTALLPARAMS="${SSM_CS_LINUX_INSTALLPARAMS//\$/\\\$}"
+declare -a INSTALLPARAMS="($SSM_CS_LINUX_INSTALLPARAMS)"
+
+for arg in "${INSTALLPARAMS[@]}"; do
+    if [[ $arg == *"--aph="* ]]; then
+        proxy_name="${arg#*=}"
+        # Remove https/http
+        proxy_name="${proxy_name//http*:'//'}"
+        echo "Proxy host $proxy_name found in arguments"
+    elif [[ $arg == *"--app="* ]]; then       
+        proxy_port="${arg#*=}"
+        echo "Proxy port $proxy_port found in arguments"
+    fi
+done
+
+if [ -n "$proxy_name" ] && [ -n "$proxy_port" ]; then
+    proxy="http://$proxy_name:$proxy_port"
+elif [ -n "$proxy_name" ]; then
+    proxy="http://$proxy_name"
+fi
+
+if [ -n "$proxy" ]; then
+    proxy="${proxy//\"}"
+    proxy="${proxy//\'}"
+    echo "Proxy settings detected in arguments, using proxy: $proxy to communicate with the CrowdStrike Apis"
+fi
+
+userAgent="crowdstrike-custom-api-distributor-package/v2.0.0"
+
+BASE_FILTER="<<SENSOR_DOWNLOAD_FILTER>>"
+BASE_URL="https://${SSM_CS_HOST}/sensors/combined/installers/v1?limit=1&sort=version"
+
+if [[ -n $SSM_CS_LINUX_VERSION ]]; then
+    FILTER="${BASE_FILTER}+version:'${SSM_CS_LINUX_VERSION}'"
+    URL="$BASE_URL&filter=$(EncodeQueryParameter "$FILTER")"
+    echo "Version specified, grabbing the exact version: ${SSM_CS_LINUX_VERSION}. Filter: ${FILTER}"
 else
-    if ! [ -z "$1" ]
-    then
-    	CS_FALCON_CLIENT_ID=$1
-    	CS_FALCON_CLIENT_SECRET=$2
-    	CS_FALCON_CID=$3
-        CS_INSTALL_PARAMS=$4
-        CS_INSTALL_TOKEN="--provisioning-token=$5"
-    fi
+    FILTER="${BASE_FILTER}"
+    URL="$BASE_URL&offset=1&filter=$(EncodeQueryParameter "$FILTER")"
+    echo "No version specified, grabbing the n-1 version that matches the base filter. Filter: ${FILTER}"
 fi
 
-if ! [ -z "$SSM_LINUX_INSTALLPARAMS" ]
-then
-    CS_INSTALL_PARAMS=${SSM_LINUX_INSTALLPARAMS}
-fi
-if ! [ -z "$SSM_INSTALLTOKEN" ]
-then
-    CS_INSTALL_TOKEN="--provisioning-token=${SSM_INSTALLTOKEN}"
+# Getting installer
+echo 'Getting installer sha256...'
+if ! _resp="$(curl -x "$proxy" --retry 15 --retry-delay 10 -sf -H "Authorization: Bearer ${SSM_CS_AUTH_TOKEN}" -H "User-Agent: $userAgent" "$URL")" ; then
+    errcho "Failed grabbing presigned url: $URL (exit status $?)..."
 fi
 
-if ! [ -z "$SSM_HOST" ]
-then
-    if [[ "$SSM_HOST" == "https://"* ]] || [[ "$SSM_HOST" == "http://"* ]]
-    then
-        SSM_HOST=$(echo $SSM_HOST | sed 's/https\?:\/\///')
-    fi
+if ! grep -q -e "name" -e "sha256" <<<"${_resp}"; then
+    errcho "No sha256 or package name returned from api call: $_resp..."
 fi
 
-if [ -z "$CS_FALCON_OAUTH_TOKEN" ] && [ -z "$CS_FALCON_CLIENT_ID" ] && [ -z "$CS_FALCON_CLIENT_SECRET" ]; then
-    echoRed "Missing Falcon OAUTH Token or Client ID and Secret Environment Variable(s)"
-    usage
+_fname="$(echo -n "${_resp}" | grep "name" | cut -d\" -f4)"
+_sha256="$(echo -n "${_resp}" | grep "sha256" | cut -d\" -f4)"
+_version="$(echo -n "${_resp}" | grep '"version"' | cut -d\" -f4)"
+
+_binary_path="${INSTALLER_LOC}/${_fname}"
+_download_url="https://$SSM_CS_HOST/sensors/entities/download-installer/v1?id=$_sha256"
+
+echo "Downloading sensor binary matching sha256 $_sha256..."
+if ! _resp="$(curl -x "$proxy" --retry 15 --retry-delay 10 -sf -H "Authorization: Bearer ${SSM_CS_AUTH_TOKEN}" -H "User-Agent: $userAgent" "$_download_url" -o "${_binary_path}")" ; then
+    errcho "Failed downloading binary: $_download_url (exit status $?)..."
+fi
+
+sensor_install_dpkg()
+{
+    INSTALLER="$(realpath "$1")"
+    RUN export DEBIAN_FRONTEND=noninteractive
+    RUN apt-get update
+    RUN apt-get -y install "$INSTALLER"
+}
+
+sensor_install_yum()
+{
+    INSTALLER="${1}"
+    sudo rpm -qa | grep falcon-sensor || yum install "${INSTALLER}" -y
+}
+
+sensor_install_zypper()
+{
+    INSTALLER="${1}"
+    sudo zypper -n --no-gpg-checks install "${INSTALLER}"
+}
+
+echo "Running install command (installing ${_binary_path}) sensor version: ${_version}..."
+
+case "<<PACKAGE_MANAGER>>" in
+  "dpkg")
+    sensor_install_dpkg "${_binary_path}"
+    ;;
+  "yum")
+    sensor_install_yum "${_binary_path}"
+    ;;
+  "zypper")
+    sensor_install_zypper "${_binary_path}"
+    ;;
+  *)
+    errcho "Unknown package manager."
     exit 1
-elif [ -z "$CS_FALCON_OAUTH_TOKEN" ]; then
-    if [ -z "$CS_FALCON_CLIENT_ID" ] || [ -z "$CS_FALCON_CLIENT_SECRET" ]; then
-        echoRed "If using CS_FALCON_CLIENT_ID and CS_FALCON_CLIENT_SECRET, both must be set."
-        exit 1
-    else
-        # Let's get a token
-        tokenResult=$(curl -X POST -s -L "https://$SSM_HOST/oauth2/token" \
-                        -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
-                        -H "User-Agent: $userAgent" \
-                        -d "client_id=$CS_FALCON_CLIENT_ID&client_secret=$CS_FALCON_CLIENT_SECRET")
-        CS_FALCON_OAUTH_TOKEN=$(echo "$tokenResult" | jsonValue "access_token" | sed 's/ *$//g' | sed 's/^ *//g')
-        if [ -z "$CS_FALCON_OAUTH_TOKEN" ]; then
-            echoRed "Unable to retrieve oauth token from api"
-            exit 1
-        fi
-    fi
-fi
-
-if [ -z "$CS_FALCON_CID" ]
-then
-    echoRed "CS_FALCON_CID must be provided in order to perform installation."
-    exit 1
-fi
-
-osDetail
-OUTPUT_DESTINATION="."
-
-case "$OS_NAME" in
-    deb?*|Deb?*|ubu?*|Ubu?* )
-        OS_NAME="Debian"
-	PACKAGER="apt"
-        ;;
-    rhel|RHEL|red?*|cent?*|Red?*|Cent?*|ora?*|Ora?* )
-        OS_NAME="RHEL/CentOS/Oracle"
-    	PACKAGER="yum"
-        ;;
-    sles|SLES )
-        OS_NAME="SLES"
-	PACKAGER="zypper"
-        ;;
-    amz?*|Ama?*|ama?* )
-        OS_NAME="Amazon Linux"
-	PACKAGER="yum"
-        ;;
-    win?*|Win?* )
-        OS_NAME="Windows"
-	PACKAGER="exe"
-        ;;
+    ;;
 esac
 
-OS_VERSION=2
-if [[ "$(uname -p)" != *86* ]]
+if [[ "$INSTALLTOKEN" ]]; then
+       echo "Passing installation token..."
+       INSTALLPARAMS+=("--provisioning-token=$INSTALLTOKEN")
+fi
+
+INSTALLPARAMS+=("--cid=$CCID")
+
+# Configure Falcon sensor
+RUN /opt/CrowdStrike/falconctl -s "${INSTALLPARAMS[@]}"
+
+# Start Falcon sensor
+if [[ -L "/sbin/init" ]]
 then
-    OS_VERSION="$OS_VERSION - arm64"
+    RUN systemctl start falcon-sensor
+else
+    RUN sudo service falcon-sensor start
 fi
 
-## Get Installer Versions
-jsonResult=$(curl -s -L -G "https://$SSM_HOST/sensors/combined/installers/v1" \
-    --data-urlencode "filter=os:\"$OS_NAME\"" \
-    -H "Authorization: Bearer $CS_FALCON_OAUTH_TOKEN" \
-    -H "User-Agent: $userAgent")
+# Sleep before verification
+sleep 5s
 
-if [[ $jsonResult == *"denied"* ]]; then
-    echoRed "Invalid Access Token"
-    exit 1
-fi
-#echo $jsonResult
-sha_list=$(echo "$jsonResult" | jsonValue "sha256")
-if [ -z "$sha_list" ]; then
-    echoRed "No sensor found for with OS Name: $OS_NAME"
-    exit 1
-fi
-
-INDEX=1
-if [ -n "$OS_VERSION" ]; then
-    found=0
-    IFS=$'\n'
-	for l in $(echo "$jsonResult" | jsonValue "os_version"); do
-        l=$(echo "$l" | sed 's/ *$//g' | sed 's/^ *//g')
-		if [ "$l" = "$OS_VERSION" ]; then
-            ((found+=1))
-            if [ "$found" == "$((NCNT+1))" ]
-            then
-	            break
-	    fi
-        fi
-		((INDEX+=1))
-    done
-    if [ $found = 0 ]; then
-        echoRed "Unable to locate matching sensor: $OS_NAME@$OS_VERSION"
-        exit 1
-    fi
-fi
-
-sha=$(echo "$jsonResult" | jsonValue "sha256" "$INDEX" | sed 's/ *$//g' | sed 's/^ *//g')
-file_type=$(echo "$jsonResult" | jsonValue "file_type" "$INDEX" | sed 's/ *$//g' | sed 's/^ *//g')
-name=$(echo "$jsonResult" | jsonValue "name" "$INDEX" | sed 's/ *$//g' | sed 's/^ *//g')
-
-if [ -z "$sha" ]; then
-    echoRed "Unable to identify a sha matching sensor: $OS_NAME@$OS_VERSION"
-    exit 1
-fi
-
-filename="$OUTPUT_DESTINATION/$name.$file_type"
-#clean up our calculated sha
-sha=$(echo $sha | xargs)
-curl -s -L "https://$SSM_HOST/sensors/entities/download-installer/v1?id=$sha" \
-    -H "Authorization: Bearer $CS_FALCON_OAUTH_TOKEN" \
-    -H "User-Agent: $userAgent" \
-    -o "$filename"
-
-echo "Sensor binary output to: $filename"
-
-if [[ "$PACKAGER" == "yum" || "$PACKAGER" == "zypper" ]]
+# Verification
+if ps -e | grep falcon-sensor &>/dev/null ;
 then
-	rpmInstall $filename $CS_FALCON_CID $CS_INSTALL_PARAMS $CS_INSTALL_TOKEN
-fi
-if [[ "$PACKAGER" == "apt" ]]
-then
-	aptInstall $filename $CS_FALCON_CID $CS_INSTALL_PARAMS $CS_INSTALL_TOKEN
+    unset SSM_CS_INSTALLTOKEN
+    unset SSM_CS_LINUX_INSTALLPARAMS
+    unset SSM_CS_CCID
+    unset SSM_CS_AUTH_TOKEN
+    unset SSM_CS_HOST
+    rm "${_binary_path}" || true
+    echo "Falcon Sensor version ${_version} installed successfully."
+else
+    errcho "Installation failed. Process falcon-sensor is not running."
+    exit 1
 fi
--- custom-api-package/package/linux/uninstall.sh
+++ custom-api-package/package/linux/uninstall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo 'Uninstalling Falcon Sensor...'
 
 # Error Handling
