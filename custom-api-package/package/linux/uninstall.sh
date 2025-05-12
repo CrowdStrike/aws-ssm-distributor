20041109f38b63c449f2a4847c138e5c32982d26
ffalor <35144141+ffalor@users.noreply.github.com>
2025-05-12 13:41:07 -0500

    add linux uninstall script

--- custom-api-package/package/linux/uninstall.sh
+++ custom-api-package/package/linux/uninstall.sh
@@ -1,49 +1,40 @@
-#! /bin/bash
+echo 'Uninstalling Falcon Sensor...'
 
-osDetail(){
-  UN=$(uname -s)
-  if [[ "$UN" == "Darwin" ]]
-  then
-	OS_NAME="Darwin"
-	OS_VERSION=$(uname -r)
-  else
-	OS_NAME=$(cat /etc/*release | grep NAME= | awk '!/CODENAME/ && !/PRETTY_NAME/' | awk '{ print $1 }' | awk -F'=' '{ print $2 }' | sed "s/\"//g")
-	OS_NAME=$(echo $OS_NAME | awk '{ print $1 }')
-	OS_VERSION=$(cat /etc/*release | grep VERSION_ID= | awk '{ print $1 }' | awk -F'=' '{ print $2 }' | sed "s/\"//g")
-  fi
+# Error Handling
+function errout {
+    rc="$?"
+    echo "[ERROR] Falcon Sensor un-installation failed with $rc while executing '$2' on line $1"
+    exit "$rc"
 }
 
-rpmUninstall(){
-   yum remove falcon-sensor -y $1
+trap 'errout "${LINENO}" "${BASH_COMMAND}" ' ERR
+
+# Echo to stderr
+function errcho {
+    >&2 echo ${@}
 }
 
-aptUninstall(){
-   apt-get -y remove falcon-sensor $1
-}
+# Starting
+aid="$(sudo /opt/CrowdStrike/falconctl -g --aid | awk -F\" '{print $2}')"
+pkg="falcon-sensor"
+echo "Running uninstall command for agent $aid ... "
 
-osDetail
-case "$OS_NAME" in
-    deb?*|Deb?*|ubu?*|Ubu?* )
-        PACKAGER="apt"
-        ;;
-    rhel|RHEL|red?*|cent?*|Red?*|Cent?*|ora?*|Ora?* )
-        PACKAGER="yum"
-        ;;
-    sles|SLES )
-        PACKAGER="zypper"
-        ;;
-    amz?*|Ama?*|ama?* )
-        PACKAGER="yum"
-        ;;
-    win?*|Win?* )
-        PACKAGER="exe"
-        ;;
-esac
-if [ "$PACKAGER" == "apt" ]
-then
-    aptUninstall $SSM_LINUX_UNINSTALLPARAMS
-elif [ "$PACKAGER" == "yum" ] || [ "$PACKAGER" == "zypper" ]
-then
-    rpmUninstall $SSM_LINUX_UNINSTALLPARAMS
+if type dnf >/dev/null 2>&1; then
+    sudo dnf remove -q -y "$pkg" || sudo rpm -e --nodeps "$pkg"
+elif type yum >/dev/null 2>&1; then
+    sudo yum remove -q -y "$pkg" || sudo rpm -e --nodeps "$pkg"
+elif type zypper >/dev/null 2>&1; then
+    sudo zypper --n remove -y "$pkg" || sudo rpm -e --nodeps "$pkg"
+elif type apt >/dev/null 2>&1; then
+    DEBIAN_FRONTEND=noninteractive sudo apt-get purge -y "$pkg"
+else
+   sudo rpm -e --nodeps "$pkg"
 fi
 
+# Verification
+if ! pgrep falcon-sensor >/dev/null; then
+    echo "Successfully finished uninstall..."
+else
+    errcho "Uninstall failed. Process falcon-sensor is still running."
+    exit 1
+fi
