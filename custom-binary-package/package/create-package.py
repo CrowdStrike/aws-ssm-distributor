5c3114953944c6bd1399f9e34709286259d5de1e
ffalor <35144141+ffalor@users.noreply.github.com>
2025-12-16 20:05:20 -0600

    add missing packages

--- custom-binary-package/package/create-package.py
+++ custom-binary-package/package/create-package.py
@@ -104,6 +104,76 @@ binary_list = [
         "installer": "yum",
     },
     {
+        "filter": "os:'*RHEL*'+os_version:'10'+platform:'linux'",
+        "path": "CS_RHEL10_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+    },
+    {
+        "filter": "os:'*RHEL*'+os_version:'10 - arm64'+platform:'linux'",
+        "path": "CS_RHEL10_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+    },
+    {
+        "path": "CS_ALMALINUX8_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'8'+platform:'linux'",
+    },
+    {
+        "path": "CS_ALMALINUX8_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'8 - arm64'+platform:'linux'",
+    },
+    {
+        "path": "CS_ALMALINUX9_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'9'+platform:'linux'",
+    },
+    {
+        "path": "CS_ALMALINUX9_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'9 - arm64'+platform:'linux'",
+    },
+    {
+        "path": "CS_ALMALINUX10_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'10'+platform:'linux'",
+    },
+    {
+        "path": "CS_ALMALINUX10_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'10 - arm64'+platform:'linux'",
+    },
+    {
+        "path": "CS_ROCKY8_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'8'+platform:'linux'",
+    },
+    {
+        "path": "CS_ROCKY8_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'8 - arm64'+platform:'linux'",
+    },
+    {
+        "path": "CS_ROCKY9_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'9'+platform:'linux'",
+    },
+    {
+        "path": "CS_ROCKY9_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'9 - arm64'+platform:'linux'",
+    },
+    {
+        "path": "CS_ROCKY10_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'10'+platform:'linux'",
+    },
+    {
+        "path": "CS_ROCKY10_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*RHEL*'+os_version:'10 - arm64'+platform:'linux'",
+    },
+    {
         "path": "CS_CENTOS8_x86_64/falcon-sensor.rpm",
         "installer": "yum",
         "filter": "os:'*CentOS*'+os_version:'8'+platform:'linux'",
@@ -114,9 +184,24 @@ binary_list = [
         "filter": "os:'*CentOS*'+os_version:'8 - arm64'+platform:'linux'",
     },
     {
+        "path": "CS_CENTOS9_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*CentOS*'+os_version:'9'+platform:'linux'",
+    },
+    {
+        "path": "CS_CENTOS10_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*CentOS*'+os_version:'10'+platform:'linux'",
+    },
+    {
         "path": "CS_ORACLE7_x86_64/falcon-sensor.rpm",
         "installer": "yum",
         "filter": "os:'*Oracle*'+os_version:'7'+platform:'linux'",
+    },
+    {
+        "path": "CS_ORACLE7_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*Oracle*'+os_version:'7 - arm64'+platform:'linux'",
     },
     {
         "path": "CS_ORACLE8_x86_64/falcon-sensor.rpm",
@@ -129,29 +214,49 @@ binary_list = [
         "filter": "os:'*Oracle*'+os_version:'9'+platform:'linux'",
     },
     {
+        "path": "CS_ORACLE10_x86_64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*Oracle*'+os_version:'10'+platform:'linux'",
+    },
+    {
+        "path": "CS_ORACLE10_ARM64/falcon-sensor.rpm",
+        "installer": "yum",
+        "filter": "os:'*Oracle*'+os_version:'10 - arm64'+platform:'linux'",
+    },
+    {
         "path": "CS_SLES12_x86_64/falcon-sensor.rpm",
         "installer": "zypper",
-        "filter": "os:'*SLES*'+os_version:'12'+platform:'linux'",
+        "filter": "os:'*SLES*'+os_version:'12'+os_version:!~'zLinux'+platform:'linux'",
     },
     {
         "path": "CS_SLES15_x86_64/falcon-sensor.rpm",
         "installer": "zypper",
-        "filter": "os:'*SLES*'+os_version:'15'+platform:'linux'",
+        "filter": "os:'*SLES*'+os_version:'15'+os_version:!~'zLinux'+platform:'linux'",
+    },
+    {
+        "path": "CS_SLES15_ARM64/falcon-sensor.rpm",
+        "installer": "zypper",
+        "filter": "os:'*SLES*'+os_version:'15 - arm64'+platform:'linux'",
     },
     {
         "path": "CS_UBUNTU_x86_64/falcon-sensor.deb",
         "installer": "dpkg",
-        "filter": "os:'*Ubuntu*'+os_version:'*16/18/20/22*'+os_version:!'*arm64*'+os_version:!~'zLinux'+platform:'linux'",
+        "filter": "os:'*Ubuntu*'+os_version:!'*arm64*'+os_version:!~'zLinux'+platform:'linux'",
     },
     {
         "path": "CS_UBUNTU_ARM64/falcon-sensor.deb",
         "installer": "dpkg",
-        "filter": "os:'*Ubuntu*'+os_version:'*18/20/22*'+os_version:~'arm64'+os_version:!~'zLinux'+platform:'linux'",
+        "filter": "os:'*Ubuntu*'+os_version:~'arm64'+os_version:!~'zLinux'+platform:'linux'",
     },
     {
         "path": "CS_DEBIAN_x86_64/falcon-sensor.deb",
         "installer": "dpkg",
-        "filter": "os:'Debian'+os_version:'*9/10/11*'+os_version:!'*arm64*'+platform:'linux'",
+        "filter": "os:'Debian'+os_version:!'*arm64*'+os_version:!~'zLinux'+platform:'linux'",
+    },
+    {
+        "path": "CS_DEBIAN_ARM64/falcon-sensor.deb",
+        "installer": "dpkg",
+        "filter": "os:'Debian'+os_version:~'arm64'+os_version:!~'zLinux'+platform:'linux'",
     },
     {
         "path": "CS_WINDOWS/WindowsSensor.exe",
@@ -168,6 +273,12 @@ falcon = APIHarness(
 print("Downloading required files...")
 
 for binary in binary_list:
+    if os.path.exists(binary["path"]):
+        print(f"Skipping download - {binary['path']} already exists")
+        os_dir = os.path.dirname(binary["path"])
+        dirs_to_delete.append(os_dir)
+        continue
+
     sensors = falcon.command(
         action="GetCombinedSensorInstallersByQuery",
         filter=binary["filter"],
